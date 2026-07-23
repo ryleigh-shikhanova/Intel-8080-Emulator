7fd46c11811dfb5aafbc9960c72c43295adef09f
Ryleigh <ryleighstefanski@gmail.com>
2026-07-23 01:14:12 -0400

    Added functions to the cpu class to help with timing information

--- i8080/include/cpu.h
+++ i8080/include/cpu.h
@@ -4,6 +4,13 @@
 #include <cstdint>
 #include "cpu_state.h"
 
+// Used to group the executed opcode along with it's t-states
+struct StepResult {
+	std::uint16_t address{}
+	std::uint8_t opcode;
+	std::uint8_t tStates;
+};
+
 class Cpu8080 {
 
 public:
@@ -13,11 +20,20 @@ public:
 	~Cpu8080() {
 
 	}
-
-	bool isFlagSet(Flag flag) const;
+	
+	[[nodiscard]] StepResult step();
+	[[nodiscard]] const CpuState& state() const noexcept
+	{
+		return state_;
+	}
+	[[nodiscard]] bool isFlagSet(Flag flag) const;
 	void setFlag(Flag flag, bool value);
 private:
 	std::uint8_t flags_ = 0x02;
+	CpuState state_{};
+
+	void executeNOP() const noexcept;
+
 };
 
 #endif
